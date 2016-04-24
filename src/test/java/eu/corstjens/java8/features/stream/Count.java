91b8c66609365a77262e2ac7d603d56bf7a3ab4e
Koen Corstjens <koencorstjens@icloud.com>
2016-04-24 15:02:02 +0200

    Add  Lambdas in the Stream examples join comma separated

--- src/test/java/eu/corstjens/java8/features/stream/Count.java
+++ src/test/java/eu/corstjens/java8/features/stream/Count.java
@@ -10,16 +10,17 @@ import java.util.function.Function;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
 
 /**
  * Created by koencorstjens on 24/04/16.
  */
 public class Count {
+    String[] words = {"test", "qaz", "test", "bla", "test", "bla"};
+    List<String> wordsList = Arrays.asList(words);
 
     @Test
     public void count() {
-        String[] words = {"test", "qaz", "test", "bla", "test", "bla"};
-        List<String> wordsList = Arrays.asList(words);
         Map<String, Long> collect =
                 wordsList.stream().collect(groupingBy(Function.identity(), counting()));
         System.out.println(collect);
@@ -27,10 +28,14 @@ public class Count {
 
     @Test
     public void sum() {
-        String[] words = {"test", "qaz", "test", "bla", "test", "bla"};
-        List<String> wordsList = Arrays.asList(words);
         long result = wordsList.stream().mapToLong(e -> 1L).sum();
         Assert.assertEquals(6l, result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void join() {
+        String result = wordsList.stream().collect(joining(" , "));
         System.out.println(result);
     }
 }
